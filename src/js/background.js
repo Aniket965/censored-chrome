053ac051a1bd4e20d013181237047554ccf3c8f1
aniket965 <aniket965.as@gmail.com>
2018-11-07 00:23:10 +0530

    added sub menus

--- src/js/background.js
+++ src/js/background.js
@@ -12,6 +12,34 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 chrome.contextMenus.create({
     id:"selectedchromecensor",
-    title: "Comprehend this text",
+    title: "Comprehend and translate",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+    id:"selectedchromecensorenglish",
+    parentId:"selectedchromecensor",
+    title: "English",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+    id:"selectedchromecensorfrench",
+    parentId:"selectedchromecensor",
+    title: "French",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+    id:"selectedchromecensorgerman",
+    parentId:"selectedchromecensor",
+    title: "German",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+    id:"selectedchromecensorjapense",
+    parentId:"selectedchromecensor",
+    title: "Japanese",
     contexts: ["selection"]
 });
