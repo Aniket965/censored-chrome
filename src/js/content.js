72d60e168c501869b760610936c673ef2fccb19e
aniket965 <aniket965.as@gmail.com>
2018-11-06 21:01:45 +0530

    added selection bubble

--- src/js/content.js
+++ src/js/content.js
@@ -20,3 +20,30 @@ function sendDataToAws(data) {
 function sendImgsToAws(data) {
     console.log(`🌄 :==> ${data}`)
 }
+
+// Add bubble to the top of the page.
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('mouseup', function (e) {
+  var selection = window.getSelection().toString();
+  if (selection.length > 0) {
+    renderBubble(e.clientX, e.clientY, selection);
+  }
+}, false);
+
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubbleDOM.style.visibility = 'hidden';
+}, false);
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+  bubbleDOM.innerHTML = "⭐️ comprehend this";
+  bubbleDOM.style.top = mouseY  + 20+ 'px';
+  bubbleDOM.style.left = mouseX + 20 + 'px';
+  bubbleDOM.style.visibility = 'visible';
+}
