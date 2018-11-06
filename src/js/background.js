b8db238d671be5caff059f1d4c435471157d6efc
aniket965 <aniket965.as@gmail.com>
2018-11-07 01:58:16 +0530

    added translate tooltip

--- src/js/background.js
+++ src/js/background.js
@@ -11,35 +11,65 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 chrome.contextMenus.create({
-    id:"selectedchromecensor",
+    id: "selectedchromecensor",
     title: "Comprehend and translate",
     contexts: ["selection"]
 });
 
 chrome.contextMenus.create({
-    id:"selectedchromecensorenglish",
-    parentId:"selectedchromecensor",
+    id: "selectedchromecensorenglish",
+    parentId: "selectedchromecensor",
     title: "English",
     contexts: ["selection"]
 });
 
 chrome.contextMenus.create({
-    id:"selectedchromecensorfrench",
-    parentId:"selectedchromecensor",
+    id: "selectedchromecensorfrench",
+    parentId: "selectedchromecensor",
     title: "French",
     contexts: ["selection"]
 });
 
 chrome.contextMenus.create({
-    id:"selectedchromecensorgerman",
-    parentId:"selectedchromecensor",
+    id: "selectedchromecensorgerman",
+    parentId: "selectedchromecensor",
     title: "German",
     contexts: ["selection"]
 });
 
 chrome.contextMenus.create({
-    id:"selectedchromecensorjapense",
-    parentId:"selectedchromecensor",
+    id: "selectedchromecensorjapense",
+    parentId: "selectedchromecensor",
     title: "Japanese",
     contexts: ["selection"]
 });
+chrome.contextMenus.onClicked.addListener(function (e) {
+    if (e.menuItemId === "selectedchromecensorjapense" && e.selectionText) {
+        let headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        fetch('https://fs22b8rq04.execute-api.us-east-1.amazonaws.com/Prod/', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({
+                "elements": [
+                    {
+                        "type": "text",
+                        "content": e.selectionText,
+                        "action": "translate",
+                        'translateTo': 'ja'
+                    },
+                ]
+            })
+        }).then(data => {
+            data.json().then(d => {
+                console.log(d);
+                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, { toolobjec: d.message[0] }, function (response) {
+  
+                    });
+                });
+
+            })
+        }).catch(err => console.log(err))
+    }
+})
--- src/js/content.js
+++ src/js/content.js
@@ -56,11 +56,21 @@ var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
 
+var toolDOM = document.createElement('div');
+toolDOM.setAttribute('class', 'tooltip__cc');
+document.body.appendChild(toolDOM);
+
+ let last_x = 0 ;
+ let last_y = 0;
 // Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
   var selection = window.getSelection().toString();
   if (selection.length > 0) {
     renderBubble(e.clientX, e.clientY, selection);
+    last_x = e.clientX;
+    last_y = e.clientY;
+    // renderToolTip(e.clientX, e.clientY, selection,"POSITIVE")
+    
   }
 }, false);
 
@@ -77,3 +87,31 @@ function renderBubble(mouseX, mouseY, selection) {
   bubbleDOM.style.left = mouseX + 20 + 'px';
   bubbleDOM.style.visibility = 'visible';
 }
+
+function renderToolTip(mouseX, mouseY, text,score) {
+let label = `<span class="p__cc" >${score}</span>`
+if(score === "NEUTRAL") {
+   label = `<span class="n__cc" >${score}</span>`
+}
+else if(score === "NEGATIVE") {
+ label = `<span class="r__cc" >${score}</span>`
+}
+else {
+  label = `<span class="p__cc" >${score}</span>`
+}
+let translated = `<div>${text}</div>`
+toolDOM.innerHTML = translated + label
+toolDOM.style.top = mouseY  +  'px';
+toolDOM.style.left = mouseX + 20 + 'px';
+toolDOM.style.visibility = 'visible';
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+           if (request.toolobjec) {
+               let objected = request.toolobjec
+            renderToolTip(last_x,last_y, objected.translated.TranslatedText,objected.score)
+           }
+    }
+);
+
